62117cf7473a514cc2bdc709c63ba174289b42a5
Thibaut SIGUENZA <tsiguenz@e2r3p6.42.fr>
2021-07-13 23:36:56 +0200

    correction c08

--- c08/ex00/ft.h
+++ c08/ex00/ft.h
@@ -6,7 +6,7 @@
 /*   By: tsiguenz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/12 19:14:17 by tsiguenz          #+#    #+#             */
-/*   Updated: 2021/07/12 20:25:58 by tsiguenz         ###   ########.fr       */
+/*   Updated: 2021/07/13 22:59:23 by tsiguenz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define FT_H
 
 void	ft_putchar(char c);
-void	ft_swap(int *a int *b);
+void	ft_swap(int *a, int *b);
 void	ft_putstr(char *str);
 int		ft_strlen(char *str);
 int		ft_strcmp(char *s1, char *s2);
--- c08/ex01/ft_boolean.h
+++ c08/ex01/ft_boolean.h
@@ -6,15 +6,15 @@
 /*   By: tsiguenz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/12 21:36:16 by tsiguenz          #+#    #+#             */
-/*   Updated: 2021/07/12 22:24:57 by tsiguenz         ###   ########.fr       */
+/*   Updated: 2021/07/13 23:10:11 by tsiguenz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_BOOLEAN_H
 # define FT_BOOLEAN_H
-# define EVEN_MSG "I have an even number of arguments."
-# define EVEN(nbr) nbr % 2
-# define ODD_MSG "I have an odd number of arguments."
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define EVEN(nbr) ((nbr % 2) ? 0 : 1)
+# define ODD_MSG "I have an odd number of arguments.\n"
 # define SUCCESS 0
 # define FALSE 0
 # define TRUE 1
